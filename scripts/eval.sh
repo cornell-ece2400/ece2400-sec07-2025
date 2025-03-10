142254c3fa53d5856f9c079c42df204e1db1d4a7
matth2k <mrh259@cornell.edu>
2025-03-09 21:23:05 -0400

    update eval script

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -18,27 +18,10 @@ if make test; then
     echo "Tests passed"
     echo "Recording execution time..."
 
-    echo "list-int-contains-eval 100" > results.txt
+    echo "selection-sort-int-eval" > results.txt
     echo "======================" >> results.txt
-    ./eval/list-int-contains-eval 100 >> results.txt
+    ./eval/selection-sort-int-eval $@ | tee -a results.txt
 
-    echo "list-int-push-back-eval 100" >> results.txt
-    echo "======================" >> results.txt
-    ./eval/list-int-push-back-eval 100 >> results.txt
-
-    echo "vector-int-contains-eval 100" >> results.txt
-    echo "======================" >> results.txt
-    ./eval/vector-int-contains-eval 100 >> results.txt
-
-    echo "vector-int-push-back-v1-eval 100" >> results.txt
-    echo "======================" >> results.txt
-    ./eval/vector-int-push-back-v1-eval 100 >> results.txt
-
-    echo "vector-int-push-back-v2-eval 100" >> results.txt
-    echo "======================" >> results.txt
-    ./eval/vector-int-push-back-v2-eval 100 >> results.txt
-
-    cat results.txt
 else
     echo "Tests failed"
     echo "SKIPPED: evaluation"
