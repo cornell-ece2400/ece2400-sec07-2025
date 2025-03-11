2c2193fbbcb308760fdaab83ed1f268536624646
matth2k <mrh259@cornell.edu>
2025-03-11 09:33:02 -0400

    Revert "remove 2 print statements"
    This reverts commit a927aa7a9e66535edcc5f817e795278404227da8.

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -16,6 +16,8 @@ if make test; then
     echo "Tests passed"
     echo "Recording execution time..."
 
+    echo "selection-sort-int-eval" > results.txt
+    echo "======================" >> results.txt
     ./build/eval/selection-sort-int-eval $@ | tee -a results.txt
 
 else
